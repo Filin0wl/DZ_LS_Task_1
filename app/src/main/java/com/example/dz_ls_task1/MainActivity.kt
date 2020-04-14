f472140e1588233d38a3f9e476490156ee1fa92a
Stacy <nastia-dogf@mail.ru>
2020-04-14 13:17:58 +0300

    add button and secondActivity

--- app/src/main/java/com/example/dz_ls_task1/MainActivity.kt
+++ app/src/main/java/com/example/dz_ls_task1/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.dz_ls_task1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private val LOG_TAG = MainActivity.javaClass.simpleName
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun launchSecondActivity(view: View) {
+        Log.d(LOG_TAG, "Button clicked!")
     }
 }
