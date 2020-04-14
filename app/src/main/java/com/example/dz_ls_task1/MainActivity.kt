67bc06659f078ff0a73d2fe0e5686165684a0c2c
Stacy <nastia-dogf@mail.ru>
2020-04-14 14:29:32 +0300

    Final version app for homework

--- app/src/main/java/com/example/dz_ls_task1/MainActivity.kt
+++ app/src/main/java/com/example/dz_ls_task1/MainActivity.kt
@@ -1,22 +1,56 @@
 package com.example.dz_ls_task1
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     companion object {
         private val LOG_TAG = MainActivity.javaClass.simpleName
-
+        const val EXTRA_MESSAGE = "com.example.android.twoactivities.extra.MESSAGE"
+        private var mMessageEditText: EditText? = null
+        const val TEXT_REQUEST = 1
+        private var mReplyHeadTextView: TextView? = null
+        private var mReplyTextView: TextView? = null
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mMessageEditText = editText_main as EditText
+        mReplyHeadTextView = text_header_reply
+        mReplyTextView = text_message_reply
+
+
     }
 
     fun launchSecondActivity(view: View) {
         Log.d(LOG_TAG, "Button clicked!")
+        val intent = Intent(this, SecondActivity::class.java)
+        val message = mMessageEditText!!.text.toString()
+        intent.putExtra(EXTRA_MESSAGE, message)
+        startActivityForResult(intent, TEXT_REQUEST)
+    }
+
+    override fun onActivityResult(
+        requestCode: Int,
+        resultCode: Int, data: Intent?
+    ) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                val reply = data!!.getStringExtra(SecondActivity.EXTRA_REPLY)
+                mReplyHeadTextView!!.visibility = View.VISIBLE
+                mReplyTextView!!.text = reply
+                mReplyTextView!!.visibility = View.VISIBLE
+            }
+        }
     }
 }
--- app/src/main/java/com/example/dz_ls_task1/SecondActivity.kt
+++ app/src/main/java/com/example/dz_ls_task1/SecondActivity.kt
@@ -1,12 +1,34 @@
 package com.example.dz_ls_task1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
+
 
 class SecondActivity : AppCompatActivity() {
-
+    companion object {
+        val EXTRA_REPLY = "com.example.android.twoactivities.extra.REPLY"
+        private var mReply: EditText? = null
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        mReply = editText_second
+        val intent = getIntent()
+        val message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE)
+        val textView = text_message
+        textView.setText(message)
+    }
+
+    fun returnReply(view: View) {
+        val reply = mReply!!.text.toString()
+        val replyIntent = Intent()
+        replyIntent.putExtra(EXTRA_REPLY, reply)
+        setResult(RESULT_OK, replyIntent)
+        finish()
     }
 }
